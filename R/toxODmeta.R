6f657dc54edc7a620cd6973efb697ecb654909b2
willgertsch <willgertsch+github@gmail.com>
2022-09-15 14:52:47 -0700

    initial main function for logistic regression

--- R/toxODmeta.R
+++ R/toxODmeta.R
@@ -2,7 +2,7 @@
 # inputs:
 # problem: list used to define optimal design problem
 # problem$model: model: logisticFP, exp, loglogistic, weibull
-# problem$beta: local parameter values
+# problem$theta: local parameter values
 # problem$obj: name of objective function
 # problem$bound: upper dose limit
 # problem$lam: dual objective parameter
@@ -13,12 +13,13 @@
 # alg_options$algorithm: name of algorithm to use
 # alg_options$iter: number of iterations
 # alg_options$swarm: swarm size
-toxODmeta = function(problem, alg_options) {
+toxODmeta = function(problem, alg_options, seed) {
 
   # process input
+  # select model
   model = problem$model
-  if (model == "logisticFP") {
-
+  if (model == "logistic") {
+    M_fun = M.logistic
   }
   else if (model == "exp") {
 
@@ -33,9 +34,39 @@ toxODmeta = function(problem, alg_options) {
     stop("Model not supported")
   }
 
-  # find optimal design
+  # select objective function
+  obj = problem$obj
+  if (obj == "D") {
+    obj_fun = obj.D
+  }
+
+  # make objective function
+  theta = problem$theta
+  obj_fun = obj_fun_factory(M_fun, obj_fun, theta)
+
+  # set up bounds
+  pts = problem$pts
+  bound = problem$bound
+  rangeVar = matrix(c(rep(c(0, bound), pts), rep(c(0,1), pts)), nrow = 2)
+
+  # set up algorithm options
+  swarm = alg_options$swarm
+  iter = alg_options$iter
+  control = list(numPopulation = swarm, maxIter = iter)
+
+  # find optimal design using metaOpt
+  algorithm = alg_options$algorithm
+  result = metaheuristicOpt::metaOpt(
+    obj_fun,
+    optimType = "MAX",
+    algorithm = algorithm,
+    numVar = 2 * pts,
+    rangeVar,
+    control,
+    seed = seed
+  )
 
   # process output
-  out = list()
+  out = result
   return(out)
 }
